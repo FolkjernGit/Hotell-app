e819a7e28965704271d5675a37524574e1011c20
Spritee <folkjern@icloud.com>
2025-11-25 11:07:03 +0100

    la till mer i booking

--- models/booking.py
+++ models/booking.py
@@ -1,12 +1,17 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, DateTime
+from datetime import datetime
+from shortcuts import s_int
 
 from models.base import Base
-from shortcuts import pkey, str_255
+from shortcuts import pkey
 
 
 class Booking(Base):
     __tablename__ = "bookings"
     id: Mapped[pkey]
-
-
+    guest_ID: Mapped[int] = mapped_column(ForeignKey("guests.id"))
+    room_ID: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
+    book_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    booked_duration: Mapped[s_int]
+    
